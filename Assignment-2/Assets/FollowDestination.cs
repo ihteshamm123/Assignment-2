8a28f2a88f997e57a12f1e595704fc8744d5b4ef
ihteshamm123 <39794328+ihteshamm123@users.noreply.github.com>
2020-05-29 02:59:28 -0700

    asg-file
    Done!

---
+++ Assignment-2/Assets/FollowDestination.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class FollowDestination : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent agent = null;
+    public Transform destination = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(destination.position);
+    }
+}
